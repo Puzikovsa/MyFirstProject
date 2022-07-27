18ee23f90346ca6ac36f18a22b90c4217a25d1f9
Pusikovsa <s.puzikov59@gmail.com>
2022-07-27 10:11:59 +0300

    Initial commit

---
+++ app/src/main/java/ru/synergy/myfirstproject/MainActivity.java
@@ -0,0 +1,20 @@
+package ru.synergy.myfirstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        Toast.makeText(this, "Я умею нажиматься", Toast.LENGTH_LONG).show();
+    }
+}
